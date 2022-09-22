fe3fb80519ea6880592da8b45cb1f87f797ce00f
23643studentdorset <23643@student.dorset-college.ie>
2022-09-22 15:08:16 +0100

    .

--- app/src/main/java/com/example/classpractice/MainActivity.kt
+++ app/src/main/java/com/example/classpractice/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.classpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.fragment.app.commit
+import com.example.classpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        val rootView = binding.root
+        setContentView(rootView)
+
+        val helloWorld = findViewById<TextView>(R.id.helloWorld)
+        Log.i("HelloWorld", helloWorld.text.toString())
+
+        binding.helloWorld.text="Hello Android"
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.main_fragment, MainFragment().apply {
+                arguments = Bundle().apply { putString("NEW_TEXT", "New Text") }
+            })
+        }
     }
 }
